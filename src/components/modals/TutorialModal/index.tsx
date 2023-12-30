90a6da6d68f52a50d88961015f01b37d23ef442d
GonAvi <dgaduff@gmail.com>
2023-12-30 00:00:53 -0600

    fix add modal

---
+++ src/components/modals/TutorialModal/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TutorialModal = () => {
+    return (
+        <div className='w-full h-full absolute top-0 left-0 '>
+
+        </div>
+    )
+}
+
+export default TutorialModal
